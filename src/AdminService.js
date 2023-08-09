96e3d6958ea50c24c0410d749aed809e5f5e6a70
Abd abuelhija <a.abd.ab@hotmail.com>
2023-08-09 23:23:52 +0300

    Updated files

--- src/AdminService.js
+++ src/AdminService.js
@@ -1,8 +1,8 @@
 import { faL } from "@fortawesome/free-solid-svg-icons";
 import axios from "axios";
 
-// const API ="https://bilal-motors.onrender.com/Admin"
-const API ="http://localhost:8000/Admin"
+const API ="https://bilal-motors.onrender.com/Admin"
+// const API ="http://localhost:8000/Admin"
 
 
 
